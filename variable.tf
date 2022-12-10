aca5091e3ab3bd6713402de1f2c8089364754238
Ye Xiong <dbaxy770928@hotmail.com>
2022-12-10 02:05:36 +0000

    argocd change

--- variable.tf
+++ variable.tf
@@ -56,7 +56,7 @@ variable "source_path" {
 
 variable "demo_dns_zone" {
   description = "Specific to your setup, pick a domain you have in route53"
-  default = "cmcloudlab446.info"
+  default = "cmcloudlab1446.info"
 
 }
 
