a80ff937b49477bdecb4e2786cea7f11ad4d3ed2
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2022-12-11 22:30:20 +1100

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -131,10 +131,10 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonEC2ContainerRegistryReadOnl
   role       = aws_iam_role.eks_nodegroup_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "eks-AmazonEC2SSM" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  role       = aws_iam_role.eks_nodegroup_role.name
-}
+#resource "aws_iam_role_policy_attachment" "eks-AmazonEC2SSM" {
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+#  role       = aws_iam_role.eks_nodegroup_role.name
+#}
 
 resource "aws_iam_role_policy_attachment" "eks_cloudwatch_container_insights" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
