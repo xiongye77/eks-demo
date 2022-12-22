639301f27444ce289f574bba4307295bd107f9c6
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2022-12-22 22:36:56 +1100

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-up-and-running-state-code-test-defrgt"
+    key            = "global/s3/terraform.tfstate"
+    dynamodb_table = "terraform-up-and-running-locks-code-test"
+    encrypt        = true
+  }
+}
