40ca3e7d51593ebbaa5d131fb739b3735fa6e86e
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2022-12-22 22:40:01 +1100

    Update k8s-ns.tf

--- k8s-ns.tf
+++ k8s-ns.tf
@@ -1,5 +1,20 @@
+resource "null_resource" "create_appmesh" {
+  depends_on = [kubernetes_namespace.k8s_ns]
+  provisioner "local-exec" {
+        command = " kubectl apply -f ./appmesh-k8sapp.yaml"
+    }
+}
+
+
 resource "kubernetes_namespace" "k8s_ns" {
+  depends_on = [helm_release.appmesh-controller]
   metadata {
     name = var.k8s_namespace
+    labels = {
+     "mesh" = "my-eks-app"
+     "appmesh.k8s.aws/sidecarInjectorWebhook" = "enabled"
+     "gateway" = "ingress-gw"
+   }
+
   }
 }
