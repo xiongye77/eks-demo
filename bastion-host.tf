6d42275511729b06f3d360761137d39c53e83921
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2023-01-06 23:16:27 +1100

    Update bastion-host.tf

--- bastion-host.tf
+++ bastion-host.tf
@@ -1,3 +1,20 @@
+data "aws_ami" "amazon-linux-2-bastion" {
+ most_recent = true
+ filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+ filter {
+   name   = "name"
+   values = ["amzn2-ami-hvm*"]
+ }
+ owners = ["137112412989"] # AWS
+}
+
 resource "aws_security_group" "eks_bastion_sg" {
   vpc_id = aws_vpc.eks-test_vpc.id
   ingress {
