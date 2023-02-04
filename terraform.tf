f055ee3c366be93297432262f7ec6bb80584a421
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2023-02-04 21:45:02 +1100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -4,5 +4,6 @@ terraform {
     key            = "global/s3/terraform.tfstate"
     dynamodb_table = "terraform-up-and-running-locks-code-test"
     encrypt        = true
+    region = "us-east-1"
   }
 }
