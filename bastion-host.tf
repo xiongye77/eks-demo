ac24504b6ba94e2b98e6334a0cd65df834a42793
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2023-02-04 21:47:19 +1100

    Update bastion-host.tf

--- bastion-host.tf
+++ bastion-host.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "bastion_host-1a" {
   #ami = "ami-09b42976632b27e9b" ami-0912f71e06545ad88 https://aws.amazon.com/amazon-linux-ami/
   ami = data.aws_ami.amazon-linux-2-bastion.id  
   instance_type = "t2.medium"
-  key_name = aws_key_pair.public_key.key_name
+  key_name = aws_key_pair.eks_public_key.key_name
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.eks_bastion_sg.id]
   subnet_id = aws_subnet.eks-test-public-1a.id
